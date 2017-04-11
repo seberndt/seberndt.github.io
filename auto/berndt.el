e5363d9aec669c7c1f86103a4e997dfc74ab2a2f
sber <sebastian.berndt@gmail.com>
2017-04-11 13:02:19 +0200

    update pubs

---
+++ auto/berndt.el
@@ -0,0 +1,14 @@
+(TeX-add-style-hook
+ "berndt"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "sea2017"
+    "DBLP:conf/aaai/BerndtLLR17"
+    "DBLP:journals/eccc/BerndtLLR17"
+    "DBLP:conf/ih/BerndtL16"
+    "DBLP:conf/isaac/BerndtL16"
+    "DBLP:conf/lata/BerndtR16"
+    "DBLP:conf/approx/BerndtJK15"
+    "DBLP:journals/corr/BerndtJK14"))
+ :bibtex)
+
