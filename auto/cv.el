1056fbfe6c7399faba386a6b4d6f38b78525aad5
Sebastian Berndt <seb@informatik.uni-kiel.de>
2019-07-22 09:04:21 +0200

    waoa

--- auto/cv.el
+++ auto/cv.el
@@ -6,12 +6,12 @@
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("ulem" "normalem") ("hyperref" "bookmarks" "colorlinks" "breaklinks" "pdftitle={Sebastian
   Berndt - vita}" "pdfauthor={Sebastian Berndt}")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -34,6 +34,7 @@
     "education"
     "publications"
     "note_phd"
+    "note_msc"
     "teaching"))
  :latex)
 
